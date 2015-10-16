b03afa38e12041f42102dfdd46774b8eb8ef979a
x44chan <chano.rocks@gmail.com>
2015-10-16 17:35:54 +0800

    css & index close to uplink

--- index.php
+++ index.php
@@ -1,24 +1,39 @@
 <?php session_start(); ?>
 <?php
-	$title="Login Page";
-	include('header.php');
+$title="Login Page";
+include('header.php');
 ?>
 <?php if(isset($_SESSION['acc_id'])){?>
-<?php if($_SESSION['level'] == 'Admin'){?>
-	<script type="text/javascript">window.location.replace("admin.php"); </script>
-<?php }else if($_SESSION['level'] == 'EMP'){?>
-	<script type="text/javascript">	window.location.replace("employee.php?ac=penot"); </script>
-<?php }else if($_SESSION['level'] == 'HR'){?>
-	<script type="text/javascript"> window.location.replace("hr.php?ac=penot"); </script>
-<?php }else if($_SESSION['level'] == 'TECH'){?>
-	<script type="text/javascript"> window.location.replace("techsupervisor.php?ac=penot"); </script>
-<?php }else{ ?>
-	<script type="text/javascript"> window.location.replace("accounting.php?ac=penot"); </script>
-<?php	}	} ?>
-		<div align = "center" style = "margin-top: 10px;">
+<?php
+if($_SESSION['level'] == 'Admin'){
+?>
+<script type="text/javascript">window.location.replace("admin.php"); </script>
+<?php
+}else if($_SESSION['level'] == 'EMP'){
+?>
+<script type="text/javascript">	window.location.replace("employee.php?ac=penot"); </script>
+<?php
+}else if($_SESSION['level'] == 'HR'){
+?>
+<script type="text/javascript"> window.location.replace("hr.php?ac=penot"); </script>
+<?php
+}else if($_SESSION['level'] == 'TECH'){
+echo '<script type="text/javascript"> window.location.replace("techsupervisor.php?ac=penot"); </script>';
+}else if($_SESSION['level'] != 'ACC'){
+?>
+<script type="text/javascript"> window.location.replace("accounting.php?ac=penot"); </script>
+<?php
+}
+}
+?>
+<style type="text/css">
+	.table {border-bottom:0px !important;}
+	.table th, .table td {border: 0px !important;}
+</style>
+<div align = "center" style = "margin-top: 10px;">
 			<img class="img-rounded" src = "img/netlink.jpg" height = "200">
 		</div>
-		<form role = "form" action = "index.php" method = "post">	
+		<form role = "form" action = "" method = "post">	
 			<table align = "center" class = "table form-horizontal" style = "margin-top: 0px; width: 800px;" >
 				<thead>
 					<tr style = "border: none;">
@@ -26,9 +41,9 @@
 					</tr>
 				</thead>
 				<tr >
-					<td><label for = "uname"><span class="glyphicon glyphicon-user"></span>  Username: </label><input placeholder = "Enter Username"id = "uname" title = "Input your username." type = "text" class = "form-control" required name = "uname"/></td>
+					<td><label for = "uname"><span class="glyphicon glyphicon-user"></span>  Username: </label><input autofocus <?php if(isset($_POST['uname'])){ echo 'value ="' . $_POST['uname'] . '"'; }?>placeholder = "Enter Username" id = "uname" title = "Input your username." type = "text" class = "form-control" required name = "uname"/></td>
 				
-					<td><label for = "pword"><span class="glyphicon glyphicon-eye-open"></span>  Password: </label><input placeholder = "Enter Password"id = "pword" title = "Input your password." type = "password" class = "form-control" required name = "password"/></td>
+					<td><label for = "pword"><span class="glyphicon glyphicon-eye-open"></span>  Password: </label><input placeholder = "Enter Password" id = "pword" title = "Input your password." type = "password" class = "form-control" required name = "password"/></td>
 				</tr>
 				<tr >
 					<td colspan = 4 align = "center" ><button style = "width: 150px; margin: auto;" type="submit" name = "submit" class="btn btn-success btn-block"><span class="glyphicon glyphicon-off"></span> Login</button></td>
@@ -40,67 +55,46 @@
 	if(isset($_POST['submit'])){
 		include('conf.php');
 		$uname = mysqli_real_escape_string($conn, $_POST['uname']);
-		$password = mysqli_real_escape_string($conn, $_POST['password']);
+		$password =  mysqli_real_escape_string($conn, $_POST['password']);
 		
 		$sql = "SELECT * FROM `login` where uname = '$uname' and pword = '$password'";
 		$result = $conn->query($sql);
 		
 		if($result->num_rows > 0){
-			while($row = $result->fetch_assoc()){
-				if($row['level'] == 'Admin'){		
-					$_SESSION['name'] = $row['fname'] . ' ' . $row['lname'];				
-					$_SESSION['level'] = $row['level'];
-					$_SESSION['acc_id'] = $row['account_id'];
-					$_SESSION['pass'] = $row['pword'];
-					$_SESSION['201date'] = $row['201date'];
-					$_SESSION['post'] = $row['position'];
-					$_SESSION['dept'] = $row['department'];
-					header("location: admin.php");
-				}else if($row['level'] == 'Employee'){
-					$_SESSION['name'] = $row['fname'] . ' ' . $row['lname'];				
-					$_SESSION['level'] = $row['level'];
-					$_SESSION['acc_id'] = $row['account_id'];
-					$_SESSION['pass'] = $row['pword'];
-					$_SESSION['201date'] = $row['201date'];
-					$_SESSION['post'] = $row['position'];
-					$_SESSION['dept'] = $row['department'];
-					$_SESSION['datehired'] = $row['edatehired'];
-					header("location: employee.php?ac=penot");		
-				}else if($row['level'] == 'HR'){
-					$_SESSION['name'] = $row['fname'] . ' ' . $row['lname'];				
-					$_SESSION['level'] = $row['level'];
-					$_SESSION['acc_id'] = $row['account_id'];
-					$_SESSION['pass'] = $row['pword'];
-					$_SESSION['201date'] = $row['201date'];
-					$_SESSION['post'] = $row['position'];
-					$_SESSION['dept'] = $row['department'];
-					$_SESSION['datehired'] = $row['edatehired'];
-					header("location: hr.php?ac=penot");		
+			while($row = $result->fetch_assoc()){					
+				$_SESSION['name'] = $row['fname'] . ' ' . $row['lname'];				
+				$_SESSION['level'] = $row['level'];
+				$_SESSION['acc_id'] = $row['account_id'];
+				$_SESSION['pass'] = $row['pword'];
+				$_SESSION['201date'] = $row['201date'];
+				$_SESSION['post'] = $row['position'];
+				$_SESSION['dept'] = $row['department'];
+				$_SESSION['datehired'] = $row['edatehired'];
+				?>
+			<?php
+				if($_SESSION['level'] == 'Admin'){
+			?>
+				<script type="text/javascript">window.location.href = "admin.php"; </script>
+			<?php
+				}else if($_SESSION['level'] == 'EMP'){
+			?>
+				<script type="text/javascript">	window.location.replace("employee.php?ac=penot"); </script>
+			<?php
+				}else if($_SESSION['level'] == 'HR'){
+			?>
+				<script type="text/javascript"> window.location.replace("hr.php?ac=penot"); </script>
+			<?php
 				}else if($row['level'] == 'TECH'){
-					$_SESSION['name'] = $row['fname'] . ' ' . $row['lname'];				
-					$_SESSION['level'] = $row['level'];
-					$_SESSION['acc_id'] = $row['account_id'];
-					$_SESSION['pass'] = $row['pword'];
-					$_SESSION['201date'] = $row['201date'];
-					$_SESSION['post'] = $row['position'];
-					$_SESSION['dept'] = $row['department'];
-					$_SESSION['datehired'] = $row['edatehired'];
-					header("location: techsupervisor.php?ac=penot");		
+				echo '<script type="text/javascript"> window.location.replace("techsupervisor.php?ac=penot"); </script>';
 				}else{
-					$_SESSION['name'] = $row['fname'] . ' ' . $row['lname'];				
-					$_SESSION['level'] = $row['level'];
-					$_SESSION['acc_id'] = $row['account_id'];
-					$_SESSION['pass'] = $row['pword'];
-					$_SESSION['201date'] = $row['201date'];
-					$_SESSION['post'] = $row['position'];
-					$_SESSION['dept'] = $row['department'];
-					$_SESSION['datehired'] = $row['edatehired'];
-					header("location: accounting.php?ac=penot");	
-				}
+			?>
+				<script type="text/javascript"> window.location.replace("accounting.php?ac=penot"); </script>
+			<?php
+				}				
 			}
 		}else{
 	echo  '<div class="alert alert-warning" align = "center">
-						<a href="#"  class="close" data-dismiss="alert" aria-label="close" >&times;</a>
+						
 						<strong>Warning!</strong> Incorrect Login.
 					</div>';
 			
